0a3af9d6b6a03b0a24796e97a4304e90750e51e2
Rahul5430 <rahul213himani@gmail.com>
2023-07-18 01:41:24 +0530

    fixed hamburger icon animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,27 +8,38 @@ import { navLinks, socialLinks } from '@/data/links';
 import useWindowSize from '@/hooks/useWindowSize';
 import logo from '@/public/white.png';
 
-const NavBtns = ({ isMobileNavOpen }: { isMobileNavOpen: boolean }) => (
-	<div
-		className={`items-start flex flex-none flex-row flex-nowrap h-min w-min justify-start max-[809px]:flex-col max-[809px]:z-[1] max-[809px]:w-full ${
-			isMobileNavOpen
-				? 'opacity-100 gap-6'
-				: 'opacity-0 gap-0 overflow-hidden h-px'
-		}`}
-	>
-		{navLinks.map(({ label, href }) => (
-			<Link
-				key={label}
-				href={href}
-				className='relative cursor-pointer no-underline'
-			>
-				<p className='hover-underline font-medium text-[#ECEDEE] text-base outline-none'>
-					{label}
-				</p>
-			</Link>
-		))}
-	</div>
-);
+const NavBtns = ({
+	isMobileNavOpen,
+	width,
+}: {
+	isMobileNavOpen: boolean;
+	width: number | undefined;
+}) => {
+	let className = '';
+	if ((width && width > 809) || isMobileNavOpen) {
+		className = 'opacity-100 gap-6';
+	} else {
+		className = 'opacity-0 gap-0 overflow-hidden h-px';
+	}
+
+	return (
+		<div
+			className={`items-start flex flex-none flex-row flex-nowrap h-min w-min justify-start max-[809px]:flex-col max-[809px]:z-[1] max-[809px]:w-full ${className}`}
+		>
+			{navLinks.map(({ label, href }) => (
+				<Link
+					key={label}
+					href={href}
+					className='relative cursor-pointer no-underline'
+				>
+					<p className='hover-underline font-medium text-[#ECEDEE] text-base outline-none'>
+						{label}
+					</p>
+				</Link>
+			))}
+		</div>
+	);
+};
 
 const AnimatedHamburgerIcon = ({
 	isMobileNavOpen,
@@ -38,7 +49,7 @@ const AnimatedHamburgerIcon = ({
 	setIsMobileNavOpen: (toggle: boolean) => void;
 }) => {
 	const spanClassName =
-		'block absolute h-px w-3.5 opacity-100 bg-[#ECEDEE] rounded-sm left-0 rotate-0 transition-all duration-[250ms]';
+		'block absolute h-px opacity-100 bg-[#ECEDEE] rounded-sm left-0 transition-all duration-[250ms]';
 
 	return (
 		// eslint-disable-next-line jsx-a11y/click-events-have-key-events
@@ -49,23 +60,27 @@ const AnimatedHamburgerIcon = ({
 			tabIndex={0}
 		>
 			<span
-				className={`${spanClassName} top-0.5 ${
-					isMobileNavOpen && 'top-[7.5px] w-0 left-1/2'
+				className={`${spanClassName} rotate-0 ${
+					isMobileNavOpen
+						? 'top-[7.5px] w-0 left-1/2'
+						: 'top-0.5 w-3.5'
 				}`}
 			/>
 			<span
-				className={`${spanClassName} top-[7.5px] ${
-					isMobileNavOpen && 'rotate-45'
+				className={`${spanClassName} w-3.5 top-[7.5px] ${
+					isMobileNavOpen ? 'rotate-45' : 'rotate-0'
 				}`}
 			/>
 			<span
-				className={`${spanClassName} top-[7.5px] ${
-					isMobileNavOpen && '-rotate-45'
+				className={`${spanClassName} w-3.5 top-[7.5px] ${
+					isMobileNavOpen ? '-rotate-45' : 'rotate-0'
 				}`}
 			/>
 			<span
-				className={`${spanClassName} top-[13px] ${
-					isMobileNavOpen && 'top-[7.5px] w-0 left-1/2'
+				className={`${spanClassName} rotate-0 ${
+					isMobileNavOpen
+						? 'top-[7.5px] w-0 left-1/2'
+						: 'top-[13px] w-3.5'
 				}`}
 			/>
 		</div>
@@ -88,14 +103,16 @@ const Navbar = () => {
 			<div className='flex-none h-[54.19px] relative overflow-visible w-full max-[809px]:flex-main max-[809px]:items-start max-[809px]:h-[54.19px] max-[809px]:justify-center max-[809px]:z-[2]'>
 				<div
 					className={`w-full rounded-xl flex-main justify-between p-4 bg-[#1C162F66] ${
-						isMobileNavOpen
+						(width && width > 809) || isMobileNavOpen
 							? 'backdrop-blur-[20px]'
 							: 'backdrop-blur-md'
 					} backdrop-blur-[20px] border border-neutral-600/20 max-[809px]:pt-[18px] max-[809px]:pb-[17px] max-[809px]:items-start`}
 				>
 					<div
 						className={`flex-main justify-start max-[809px]:flex-col max-[809px]:z-[1] max-[809px]:w-px max-[809px]:grow max-[809px]:items-start ${
-							isMobileNavOpen ? 'gap-6' : 'gap-0'
+							(width && width > 809) || isMobileNavOpen
+								? 'gap-6'
+								: 'gap-0'
 						}`}
 					>
 						<div className='w-7 h-auto'>
@@ -103,7 +120,10 @@ const Navbar = () => {
 								<Image src={logo} alt='Logo' />
 							</Link>
 						</div>
-						<NavBtns isMobileNavOpen={isMobileNavOpen} />
+						<NavBtns
+							isMobileNavOpen={isMobileNavOpen}
+							width={width}
+						/>
 					</div>
 					<div className='flex-main justify-start gap-3'>
 						<div className='flex-main justify-center gap-4'>
