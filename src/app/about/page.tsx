0534c57e94922e59772989b1b02850e048719a54
Rahul Sharma <67220225+Rahul5430@users.noreply.github.com>
2024-05-20 00:55:55 +0530

    about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
-// import Image from 'next/image';
+import Image from 'next/image';
 
-// import profilePic from '@/assets/rahul.JPG';
+import profilePic from '@/assets/rahul.JPG';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import PageHeader from '@/components/PageHeader';
@@ -11,11 +11,21 @@ export default function Work() {
 		<>
 			<Navbar />
 			<PageHeader data={pageData.about} />
-			<div className=''>
-				{/* <Image src={profilePic} alt='Picture of me' />
+			<div className='flex flex-row items-center justify-evenly p-4 max-tablet:flex-col-reverse'>
 				<p className='speech-bubble'>
-					Hello, <br /> World!
-				</p> */}
+					Hi, I&apos;m Rahul! As a full-stack cross-platform
+					developer, I blend creativity with technical expertise to
+					craft seamless, innovative, and user-friendly applications.
+					With a deep passion for technology and a dedication to
+					open-source contributions, I am committed to pushing the
+					boundaries of what's possible in software development.
+				</p>
+				<Image
+					src={profilePic}
+					alt='Picture of me'
+					className='h-auto w-[300px] cursor-pointer rounded-[11px] shadow-[5px_10px_18px_rgba(136,136,136,1)] transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-[5px_10px_18px_rgba(59,89,152,1)]'
+					priority
+				/>
 			</div>
 			<Footer />
 		</>
